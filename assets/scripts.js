1646c1939954dac7c8afcf4376bec1de9da4f518
chris green <greench@gmail.com>
2019-02-23 14:22:59 -0800

    new files

--- assets/scripts.js
+++ assets/scripts.js
@@ -270,11 +270,37 @@ $(document).ready(function () {
   document.getElementById("save").addEventListener("click", function(){
 // div block id
 console.log(divAmount[0]);
+
 let blockoValue = [];
 let rotateoValue = [];
-// let colorV = document.getElementById('b0');
+let totalValue = [];
 
-// console.log(element);
+
+if (divAmount[0] == 16) {
+  totalValue.push('0, 250, 500, 750, 1000');
+}
+
+else if (divAmount[0] == 25) {
+  totalValue.push('0,200,400,600,800,1000');
+}
+
+else if (divAmount[0] == 36) {
+  totalValue.push('0, 166.6666, 333.3333, 500, 666.6666, 833.3333,1000');
+}
+
+else if (divAmount[0] == 49) {
+  totalValue.push('0, 142.857, 285.714, 428.571, 571.428, 714.285, 857.142, 1000');
+}
+
+else if (divAmount[0] == 64) {
+  totalValue.push('0, 125, 250, 375, 500, 625, 750, 875, 1000');
+}
+
+else if (divAmount[0] == 81) {
+  totalValue.push('0, 111.1111, 222.2222, 333.3333, 444.4444, 555.5555, 666.6666, 777.7777, 888.8888, 1000');
+}
+
+
 
 for (i = 0; i < divAmount[0]; i++) {
 let blocko = [];
@@ -284,7 +310,6 @@ let element = document.getElementById('b'+ i);
 //   
 // svg block number
 let bData = element.outerHTML;
-
 let svgBlockData = bData.substring(
   bData.lastIndexOf('<svg id="') + 14, 
   bData.lastIndexOf('" "class')
@@ -299,17 +324,17 @@ let svgBlockData = bData.substring(
 
 rotateo.push(rotateData);
 
-// console.log(blocko);
 blockoValue.push(blocko);
 rotateoValue.push(rotateo);
-
 }
+
+
 console.log(blockoValue);
 console.log(rotateoValue);
 
 document.getElementById('block-data').innerHTML = blockoValue;
 document.getElementById('rotate-data').innerHTML = rotateoValue;
-
+document.getElementById('total-data').innerHTML = totalValue;
   });
 
   //========================================================================================//
