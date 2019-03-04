3f1691d7ada16b6a66533468b3f7524c871b84b8
chris green <greench@gmail.com>
2019-03-04 11:49:08 -0800

    new svg files

--- assets/scripts-gallery.js
+++ assets/scripts-gallery.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         '<div id="" class="boxes-card shape-thumbnail"><svg id="block' + i + '" "class="" viewBox="0 0 200 200"></svg></div>');
       $('#boxes-holder').append(total);
     }
-    console.log('blocksforhtml func');
+
   }
 
   blocksForHtml(totalShapes);
@@ -30,7 +30,7 @@ $(document).ready(function () {
       // attributes.attr({ fill: dColor1 });
       block.append(paper);
     });
-    console.log('loadBlocks func');
+
   }
 
   b0 = Snap('#block0'); b1 = Snap('#block1'); b2 = Snap('#block2'); b3 = Snap('#block3'); b4 = Snap('#block4'); b5 = Snap('#block5'); b6 = Snap('#block6'); b7 = Snap('#block7'); b8 = Snap('#block8'); b9 = Snap('#block9'); b10 = Snap('#block10'); b11 = Snap('#block11'); b12 = Snap('#block12'); b13 = Snap('#block13'); b14 = Snap('#block14'); b15 = Snap('#block15'); b16 = Snap('#block16'); b17 = Snap('#block17'); b18 = Snap('#block18'); b19 = Snap('#block19'); b20 = Snap('#block20'); b21 = Snap('#block21'); b22 = Snap('#block22'); b23 = Snap('#block23'); b24 = Snap('#block24'); b25 = Snap('#block25'); b26 = Snap('#block26'); b27 = Snap('#block27'); b28 = Snap('#block28'); b29 = Snap('#block29'); b30 = Snap('#block30'); b31 = Snap('#block31'); b32 = Snap('#block32'); b33 = Snap('#block33'); b34 = Snap('#block34'); b35 = Snap('#block35'); b36 = Snap('#block36'); b37 = Snap('#block37'); b38 = Snap('#block38'); b39 = Snap('#block39'); b40 = Snap('#block40'); b41 = Snap('#block41'); b42 = Snap('#block42'); b43 = Snap('#block43'); b44 = Snap('#block44'); b45 = Snap('#block45'); b46 = Snap('#block46'); b47 = Snap('#block47'); b48 = Snap('#block48'); b49 = Snap('#block49'); b50 = Snap('#block50'); b51 = Snap('#block51'); b52 = Snap('#block52'); b53 = Snap('#block53'); b54 = Snap('#block54'); b55 = Snap('#block55'); b56 = Snap('#block56'); b57 = Snap('#block57'); b58 = Snap('#block58'); b59 = Snap('#block59'); b60 = Snap('#block60'); b61 = Snap('#block61'); b62 = Snap('#block62'); b63 = Snap('#block63'); b64 = Snap('#block64'); b65 = Snap('#block65'); b66 = Snap('#block66'); b67 = Snap('#block67'); b68 = Snap('#block68'); b69 = Snap('#block69'); b70 = Snap('#block70'); b71 = Snap('#block71'); b72 = Snap('#block72'); b73 = Snap('#block73'); b74 = Snap('#block74'); b75 = Snap('#block75'); b76 = Snap('#block76'); b77 = Snap('#block77'); b78 = Snap('#block78'); b79 = Snap('#block79'); b80 = Snap('#block80'); b81 = Snap('#block81'); 
@@ -56,12 +56,11 @@ let bData = [
     function makeBlocks(b) {
       setTimeout( function(){
       for (i = 0; i < bData.length; i++) {
-        var block = $(
-          "<div class='0b" + i + " block-up data rotate " + b + "' style='transform: rotate(" + rData[i] + "deg);'></div>");
+        var block = $("<div class='0b" + i + " block-up data rotate " + b + "' style='transform: rotate(" + rData[i] + "deg);'></div>");
         $('.art-board').append(block);
         $('#block' + bData[i] + '').clone().appendTo(".0b" + i + "");
       }
-      console.log('makeBlocks func');
+      
     }, 1 );
     }
 
@@ -82,7 +81,7 @@ function makeBlocks2(b) {
     $('.art-board2').append(block);
     $('#block' + bData[i] + '').clone().appendTo(".1b" + i + "");
   }
-  console.log('makeBlocks func');
+
 }, 1 );
 }
 
