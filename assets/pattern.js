f1d967695d37ca42260979b087320b88b2853a2f
greench0 <greench@gmail.com>
2019-10-30 22:10:22 -0700

    block rearrange

--- assets/pattern.js
+++ assets/pattern.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   let svgId = "block16";
 
   // a = how many svg files
-  const totalShapes = 125;
+  const totalShapes = 131;
 
   color = '333333';
   color2 = 'FFDD79';
