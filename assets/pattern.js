3047ced765ecf3a41ee84fe90a9c6b4f91ae7114
greench0 <greench@gmail.com>
2019-11-29 21:43:03 -0800

    blocks

--- assets/pattern.js
+++ assets/pattern.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   let svgId = "block16";
 
   // a = how many svg files
-  const totalShapes = 167;
+  const totalShapes = 169;
 
   color = '151515';
   color2 = 'FFCE00';
