7a023458ffdd5b4dbb769f5cdf970d57fb084b45
greench0 <greench@gmail.com>
2020-02-27 17:18:13 -0800

    bloxsx xxx

--- assets/pattern.js
+++ assets/pattern.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   let svgId = "block16";
 
   // a = how many svg files
-  const totalShapes = 180;
+  const totalShapes = 182;
 
   color = '151515';
   color2 = 'FFCE00';
