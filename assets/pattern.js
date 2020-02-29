27a8409f5ed23e89e3f7bc051b8d34e4d0a6aca5
greench0 <greench@gmail.com>
2020-02-28 17:17:14 -0800

    dflskfja

--- assets/pattern.js
+++ assets/pattern.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   let svgId = "block16";
 
   // a = how many svg files
-  const totalShapes = 184;
+  const totalShapes = 196;
 
   color = '151515';
   color2 = 'FFCE00';
